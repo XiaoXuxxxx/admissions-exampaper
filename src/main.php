bba0d24bf7f35db7a36bd8c7e62b8ce83e5eed20
waranat suttikarn <waranat.cpe@gmail.com>
2022-02-20 02:41:58 +0700

    fix: js store file

--- src/main.php
+++ src/main.php
@@ -413,6 +413,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap.min.js"></script>
-<script type="text/javascript" src="./js/app.js"></script>
+<script type="text/javascript" src="./js/mapp.js"></script>
 </body>
 </html>
